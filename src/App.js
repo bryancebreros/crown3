9a1179d8ce1de3ceb572a1b533c164e98f5cf1ae
Bryan Sandoval <cebrerosbryan@gmail.com>
2022-11-02 01:01:41 -0700

    checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authentication/authentication.component'
 import Shop from './components/routes/shop/shop.component';
+import Checkout from './components/routes/checkout/checkout.component';
 const App = () => {
   return (
     <Routes>
@@ -10,6 +11,8 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element={<Checkout />} />
+
       </Route>
     </Routes>
   );
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {CartContext} from '../../contexts/cart.context'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
@@ -7,6 +8,11 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckout = () => {
+        navigate('/checkout')
+    }
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -14,10 +20,10 @@ const CartDropdown = () => {
                     <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckout}>GO TO CHECKOUT</Button>
 
         </div>
     )
 }
 
-export default CartDropdown
+export default CartDropdown;
